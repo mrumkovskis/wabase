ee1c768e6a6d845505367cf6892d9f3d72dba41e
Martins Rumkovskis <martins@uniso.lv>
2020-09-12 20:24:48 +0300

    java properties resource loading in utf-8 encoding

--- src/main/scala/I18n.scala
+++ src/main/scala/I18n.scala
@@ -1,6 +1,7 @@
 package org.wabase
 
-import java.util.{Locale, ResourceBundle}
+import java.util
+import java.util.{Locale, PropertyResourceBundle, ResourceBundle}
 
 //for scala 2.12 compatibility
 import scala.collection.convert.ImplicitConversions.`enumeration AsScalaIterator`
@@ -13,7 +14,52 @@ trait I18n {
   /** Subclass can override this value */
   val ResourceName = WabaseResourceName
 
-  def bundle(name: String)(implicit locale: Locale): ResourceBundle = ResourceBundle.getBundle(name, locale)
+  /** java.properties resource loader in utf-8 encoding,
+    * until java 8 (including) properties by default are loaded in ISO 8859-1 encoding */
+  private lazy val loaderControl = if (System.getProperty("java.version") >= "1.9") null else {
+    new ResourceBundle.Control {
+      override def newBundle(baseName: String,
+                             locale: Locale,
+                             format: String,
+                             loader: ClassLoader,
+                             reload: Boolean): ResourceBundle = {
+        if (baseName == null || locale == null || format == null || loader == null)
+          throw new NullPointerException()
+        if (ResourceBundle.Control.FORMAT_PROPERTIES.contains(format)) {
+          import java.io._
+          import java.net._
+          val bundleName: String = toBundleName(baseName, locale)
+          val resourceName: String = toResourceName(bundleName, "properties")
+          val stream = if (reload) {
+            val url: URL = loader.getResource(resourceName)
+            if (url != null) {
+              val connection: URLConnection = url.openConnection()
+              if (connection != null) {
+                // Disable caches to get fresh data for
+                // reloading.
+                connection.setUseCaches(false)
+                connection.getInputStream()
+              } else null
+            } else null
+          } else {
+            loader.getResourceAsStream(resourceName)
+          }
+          if (stream != null) {
+            val br = new BufferedReader(new InputStreamReader(stream, "UTF-8"))
+            val bundle = new PropertyResourceBundle(br)
+            br.close()
+            bundle
+          } else null
+        } else {
+          super.newBundle(baseName, locale, format, loader, reload)
+        }
+      }
+    }
+  }
+
+  def bundle(name: String)(implicit locale: Locale): ResourceBundle = Option(loaderControl)
+    .map(ResourceBundle.getBundle(name, locale, _))
+    .getOrElse(ResourceBundle.getBundle(name, locale))
 
   def translate(str: String, params: String*)(implicit locale: Locale): String = {
     translateFromBundle(ResourceName, str, params: _*)
