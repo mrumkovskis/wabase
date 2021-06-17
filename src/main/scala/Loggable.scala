5aa33bf6ec1b3b65c6a260ca3daf4f23b5f1f80a
Martins Rumkovskis <martins@uniso.lv>
2021-06-17 17:25:33 +0300

    formatting, cleanup

--- src/main/scala/Loggable.scala
+++ src/main/scala/Loggable.scala
@@ -1,9 +1,9 @@
 package org.wabase
 
-import com.typesafe.scalalogging.{Logger, LazyLogging}
-import org.slf4j.{LoggerFactory, Marker}
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
-trait Loggable{
+trait Loggable {
   protected lazy val logger: Logger = {
     val name = getClass.getName
     val nameUpdated = if (name.endsWith("$")) name.substring(0, name.length - 1) else name
