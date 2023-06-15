3642d5f2aaf6046cac449a162768a10d88623175
Guntis Ozols <guntiso@uniso.lv>
2023-06-15 23:37:03 +0300

    more support for OffsetDateTime - result decoder

--- src/main/scala/RequestDecoder.scala
+++ src/main/scala/RequestDecoder.scala
@@ -33,6 +33,7 @@ class CborOrJsonDecoder(typeDefs: Seq[TypeDef], nameToViewDef: Map[String, ViewD
       case "java.time.LocalDate"      => localDateDecoder
       case "java.time.LocalTime"      => localTimeDecoder
       case "java.time.LocalDateTime"  => localDateTimeDecoder
+      case "java.time.OffsetDateTime" => offsetDateTimeDecoder
       case "scala.math.BigInt"     => Decoder.forBigInt
       case "scala.math.BigDecimal" => Decoder.forBigDecimal
       case "java.lang.Double"   => Decoder.forBoxedDouble
