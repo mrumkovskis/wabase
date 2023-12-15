ef58562691dc042ca1b5494c02f694c9dc4032df
Guntis Ozols <guntiso@uniso.lv>
2023-12-15 02:24:12 +0200

    date and datetime handling and formatting improvements for xlsxml streaming

--- src/main/scala/spreadsheet/XlsXmlStreamer.scala
+++ src/main/scala/spreadsheet/XlsXmlStreamer.scala
@@ -1,6 +1,8 @@
 package org.wabase.spreadsheet.xlsxml
 
 import java.io._
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDateTime, ZoneOffset}
 import org.wabase.Format
 
 case class Font(bold: Boolean) {
@@ -22,7 +24,11 @@ case class NumberFormat(format: String) {
   }
 }
 object NumberFormat {
-  val DATE = NumberFormat("dd.mm.yyyy;@")
+  //? DATE_DMY          = NumberFormat("""dd.mm.yyyy;@""")
+  val DATE_YMD          = NumberFormat("""yyyy\-mm\-dd""")
+  val DATE_TIME_HM      = NumberFormat("""yyyy\-mm\-dd\ hh:mm""")
+  val DATE_TIME_HMS     = NumberFormat("""yyyy\-mm\-dd\ hh:mm:ss""")
+  val DATE_TIME_HMS_MS  = NumberFormat("""yyyy\-mm\-dd\ hh:mm:ss.000""")
 }
 case class Style(id: String, numberFormat: NumberFormat = null, font: Font = null) {
   require(id != null, "Style id must not be null")
@@ -46,6 +52,8 @@ case object STRING extends DataType { val name = "String" }
 case class Cell(type_ : DataType, value: String, style: Style)
 
 class XlsXmlStreamer(val out: Writer) {
+  private val xlsXmlDateTimeFormatter =
+    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
 
   def startWorkbook(styles: Seq[Style]): Unit = {
     out write """  |<?xml version="1.0" encoding="UTF-8"?>
@@ -81,9 +89,24 @@ class XlsXmlStreamer(val out: Writer) {
       case n: Number =>
         t = NUMBER
         v = n.toString
+      case d: java.time.LocalDate =>
+        t = DATE_TIME
+        v = d.atStartOfDay.format(xlsXmlDateTimeFormatter)
+      case d: java.time.LocalDateTime =>
+        t = DATE_TIME
+        v = d.format(xlsXmlDateTimeFormatter)
+      case d: java.sql.Timestamp =>
+        t = DATE_TIME
+        v = d.toLocalDateTime.format(xlsXmlDateTimeFormatter)
+      case d: java.sql.Date =>
+        t = DATE_TIME
+        v = d.toLocalDate.atStartOfDay.format(xlsXmlDateTimeFormatter)
+      case d: java.time.Instant =>
+        t = DATE_TIME
+        v = LocalDateTime.ofInstant(d, ZoneOffset.UTC).format(xlsXmlDateTimeFormatter)
       case d: java.util.Date =>
         t = DATE_TIME
-        v = Format.xsdDate(d) + "T00:00:00.000" //for example xlsxDateTime(d) add 1 hour to specific dates, like '1984-04-01'
+        v = LocalDateTime.ofInstant(d.toInstant, ZoneOffset.UTC).format(xlsXmlDateTimeFormatter)
       case x =>
         t = STRING
         v = Format.xmlEscape(x.toString) // TODO do not build escaped string, stream it!
