0b33d78be1a6a1fddac118813087d2e51d5086ef
Martins Rumkovskis <martins@uniso.lv>
2024-12-24 00:17:28 +0200

    tresql uri wip

--- src/test/scala/TresqlUriSpecs.scala
+++ src/test/scala/TresqlUriSpecs.scala
@@ -13,23 +13,28 @@ class TresqlUriSpecs extends AnyFlatSpec with Matchers {
     DbDrivers.loadDrivers
     var conn: Connection = DriverManager.getConnection("jdbc:hsqldb:mem:tresql_uri")
     val res = new Resources{}.withConn(conn).withDialect(org.tresql.dialects.HSQLDialect)
-    val tresql_uris = List[(String, Map[String, Any], TresqlUri.Uri)](
+    val tresql_uris = List[(String, Map[String, Any], TresqlUri.Uri, String)](
       ("{ 'path1', 'path2', '?', 'value1' param1, 'value2' param2 }",
         Map(),
-        TresqlUri.Uri("path1", List("path2"), ListMap("param1" -> "value1", "param2" -> "value2"))
+        TresqlUri.Uri("path1", List("path2"), ListMap("param1" -> "value1", "param2" -> "value2")),
+        "x"
       ),
       ("{ :path1?, :path2?, '?', :param1? param1, :param2? param2 }",
         Map("path2" -> "path2", "param1" -> "value1"),
-        TresqlUri.Uri("path2", List(), ListMap("param1" -> "value1"))
+        TresqlUri.Uri("path2", List(), ListMap("param1" -> "value1")),
+        "x"
       ),
       ("{ :path1?, :path2?, :path3?, '?', :param1? param1, :param2? param2 }",
         Map("path2" -> "path2", "path3" -> "path3"),
-        TresqlUri.Uri("path2", List("path3"), ListMap())
+        TresqlUri.Uri("path2", List("path3"), ListMap()),
+        "x"
       )
     )
-    tresql_uris foreach { case (uriTresql, bind_vars, uri) =>
+    tresql_uris foreach { case (uriTresql, bind_vars, truri, uri) =>
       val turi = new TresqlUri().tresqlUriValue(TresqlUri.Tresql(uriTresql))(TresqlQuery, bind_vars, res)
-      turi shouldBe uri
+      turi shouldBe truri
+      //TODO test http uris
+      //new TresqlUri().uri(turi).toString() shouldBe uri
     }
   }
 }
