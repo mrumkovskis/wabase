868ba4218074c85be72b770b9c9ee8b3b4ad062d
Guntis Ozols <guntiso@uniso.lv>
2025-02-01 23:48:33 +0200

    ComponentConf

---
+++ src/main/scala/ComponentConf.scala
@@ -0,0 +1,54 @@
+package org.wabase
+
+import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigResolveOptions, ConfigValueType}
+
+import scala.jdk.CollectionConverters._
+
+object ComponentConf {
+
+  private lazy val defaultOverrides           = ConfigFactory.defaultOverrides()
+  private lazy val defaultApplication         = ConfigFactory.defaultApplication()
+  private lazy val defaultReferenceUnresolved = ConfigFactory.defaultReferenceUnresolved()
+
+  /**
+   * Settings for child conf are prioritized over settings from parent conf.
+   * 1. Tunable settings from props and confs (application.*, [dedicated conf], reference.conf)
+   * 2. Settings in [dedicated conf] (if resource with requested name is in classpath)
+   * 3. Settings in props and application confs (application.*, reference.conf)
+   */
+  def getConfigs(
+    dedicatedConfResourceName: String,
+    parentConfPath: String,
+    tunablePaths: Set[String],
+  ): Seq[(String, Config)] = {
+
+    val dedicLoad = ConfigFactory.parseResources(dedicatedConfResourceName)
+    val dedicConf = dedicLoad.resolve(ConfigResolveOptions.noSystem())
+
+    val tunedConf = defaultOverrides
+        .withFallback(defaultApplication)
+        .withFallback(dedicLoad)
+        .withFallback(defaultReferenceUnresolved)
+        .resolve()
+
+    /* Removes tunable tuned (i.e. found in tuned conf) paths from dedicated conf */
+    def excludeTunable(dedicConf: Config): Config =
+      tunablePaths.foldLeft(dedicConf)(_ withoutPath _)
+
+    val r_path    = parentConfPath
+    val tunedCfgR =
+      if (tunedConf.hasPath(r_path)) tunedConf.getConfig(r_path) else ConfigFactory.empty
+    val dedicCfgR = excludeTunable(
+      if (dedicConf.hasPath(r_path)) dedicConf.getConfig(r_path) else ConfigFactory.empty
+    )
+
+    dedicCfgR.root().asScala
+      .collect { case (n, v) if v.valueType() == ConfigValueType.OBJECT =>
+        val childConf =
+          excludeTunable(v.asInstanceOf[ConfigObject].toConfig)
+            .withFallback(dedicCfgR)
+            .withFallback(tunedCfgR.getConfig(n).withFallback(tunedCfgR))
+        n -> childConf
+      }.toSeq
+  }
+}
