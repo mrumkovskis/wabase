2c5dc6785a6b9811af7cae89b54eebac34491979
Martins Rumkovskis <martins@uniso.lv>
2025-03-13 11:41:15 +0200

    refactoring - class WabaseServer

--- src/main/scala/WabaseServer.scala
+++ src/main/scala/WabaseServer.scala
@@ -8,40 +8,44 @@ import scala.collection.immutable.Seq
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
+class WabaseServer {
+  val port = config.getInt("port")
+
+  implicit val serverSystem: ActorSystem  = ActorSystem("wabase-server")
+  implicit val ec: ExecutionContext = serverSystem.dispatcher
+  val executionImpl = new ExecutionImpl()(serverSystem)
+
+  val wabase: WabaseService.Wabase = new WabaseApp[WabaseUser]
+    with Execution
+    with AppBase[WabaseUser]
+    with NoAudit[WabaseUser]
+    with DbAccess
+    with NoAuthorization[WabaseUser]
+    with NoCustomConstraintMessage
+    with NoValidation
+    with Marshalling
+    with AppProvider[WabaseUser]
+    with JsonConverterProvider
+  {
+    // Members declared in org.wabase.Execution
+    override protected def execution: org.wabase.Execution = executionImpl
+
+    // Members declared in org.wabase.AppProvider
+    override type App = AppBase[WabaseUser]
+    override protected def initApp: App = this
+
+    // Members declared in org.wabase.JsonConverterProvider
+    override protected def initJsonConverter: org.wabase.JsonConverter[?] = qio
+  }
+
+  val deferredControl = new WabaseDeferredControl(wabase)
+  val service         = new WabaseService
+}
+
 object WabaseServer {
   def main(args: Array[String]): Unit = {
-    val port = config.getInt("port")
-
-    implicit val serverSystem: ActorSystem  = ActorSystem("wabase-server")
-    implicit val ec: ExecutionContext = serverSystem.dispatcher
-    val executionImpl = new ExecutionImpl()(serverSystem)
-
-    val wabase: WabaseService.Wabase = new WabaseApp[WabaseUser]
-        with Execution
-        with AppBase[WabaseUser]
-        with NoAudit[WabaseUser]
-        with DbAccess
-        with NoAuthorization[WabaseUser]
-        with NoCustomConstraintMessage
-        with NoValidation
-        with Marshalling
-        with AppProvider[WabaseUser]
-        with JsonConverterProvider
-    {
-      // Members declared in org.wabase.Execution
-      override protected def execution: org.wabase.Execution = executionImpl
-
-      // Members declared in org.wabase.AppProvider
-      override type App = AppBase[WabaseUser]
-      override protected def initApp: App = this
-
-      // Members declared in org.wabase.JsonConverterProvider
-      override protected def initJsonConverter: org.wabase.JsonConverter[?] = qio
-    }
-
-    val deferredControl = new WabaseDeferredControl(wabase)
-    val service         = new WabaseService
-
+    val server = new WabaseServer
+    import server._
     // TODO support TLS if configured
     val bindingFuture = Http().newServerAt("0.0.0.0", port).bind(service.handle(wabase, deferredControl))
 
