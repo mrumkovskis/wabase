81f8b33a03c69ae2ddc57407dcae693c504f9704
Guntis Ozols <guntiso@uniso.lv>
2025-10-14 00:56:31 +0300

    integration tests - audit sample for new flow improvements

--- src/it/scala/Audit.scala
+++ src/it/scala/Audit.scala
@@ -1,11 +1,13 @@
 package wabase.app
 
 
-import io.bullet.borer.{Encoder, Json}
 import io.bullet.borer.compat.pekko._
 import io.bullet.borer.derivation.MapBasedCodecs._
+import io.bullet.borer.{Encoder, Json}
+import org.apache.pekko.NotUsed
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.http.scaladsl.model.{HttpEntity, HttpHeader, HttpResponse}
+import org.apache.pekko.stream.scaladsl._
 import org.apache.pekko.util.ByteString
 import org.mojoz.querease.{QuereaseIo, SaveMethod}
 import org.wabase.WabaseAppConfig.DefaultCp
@@ -15,11 +17,15 @@ import org.wabase.{BufferedAudit, CborOrJsonAnyValueDecoder, DbAccess, DefaultAp
 
 import java.nio.file.Files
 import java.time.Instant
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
 
 object Audit extends Loggable {
   implicit val auditPoolName: PoolName = PoolName("wabase_it_audit_cp")
+
+  // Ensure AuditRecord (req + resp + etc) fits within bufferedAudit.reader.maxRecordSize!
+  private val maxContentSizeToAudit: Long = 256 * 1024
+
   case class RequestAudit(
     uri:      String,
     method:   String,
@@ -120,16 +126,102 @@ object Audit extends Loggable {
     bufferedAudit.writer.writeRecord(serialized)
   }
 
+  private def shouldCaptureAndPromise(contentLengthOption: Option[Long]): (Boolean, Promise[ByteString]) = {
+    val promise = Promise[ByteString]()
+    contentLengthOption match {
+      case Some(len) if len > maxContentSizeToAudit =>
+        promise.success(ByteString(s"[large content: $len bytes]"))
+        (false, promise)
+      case Some(0) =>
+        promise.success(ByteString.empty)
+        (false, promise)
+      case _ => (true, promise)
+    }
+  }
+
+  private def createCaptureSink(promise: Promise[ByteString])(implicit ec: ExecutionContext): Sink[ByteString, NotUsed] = {
+    Sink.fold[(Long, ByteString), ByteString]((0L, ByteString.empty)) { case ((size, content), bs) =>
+      val added = bs.length.toLong
+      val newSize = size + added
+      if (size >= maxContentSizeToAudit) {
+        (newSize, content)
+      } else {
+        val remain = maxContentSizeToAudit - size
+        if (added <= remain) {
+          (newSize, content ++ bs)
+        } else {
+          (newSize, content ++ bs.take(remain.toInt))
+        }
+      }
+    }.mapMaterializedValue { fut =>
+      fut.foreach { case (size, content) =>
+        val bs = if (size > maxContentSizeToAudit) ByteString(s"[large content: $size bytes]") else content
+        promise.success(bs)
+      }
+      NotUsed
+    }
+  }
+
   def audit(innerHandler: RequestHandler): RequestHandler = ctx => {
     val ts = Instant.now
-    val innerCtx = ctx
-    innerHandler(innerCtx).andThen {
+    val (attachReqCapture, reqPromise) = shouldCaptureAndPromise(ctx.req.entity.contentLengthOption)
+
+    val reqDataBytes = if (attachReqCapture) {
+      val sink = createCaptureSink(reqPromise)
+      ctx.req.entity.dataBytes.alsoTo(sink)
+    } else {
+      ctx.req.entity.dataBytes
+    }
+
+    val modRequest = ctx.req.withEntity(
+      ctx.req.entity.contentLengthOption match {
+        case Some(0)    => ctx.req.entity
+        case Some(len)  => HttpEntity.Default(ctx.req.entity.contentType, len, reqDataBytes)
+        case None       => HttpEntity        (ctx.req.entity.contentType, reqDataBytes)
+      }
+    )
+
+    val innerCtx = ctx.copy(req = modRequest)
+
+    innerHandler(innerCtx).transform {
       case Success(response) =>
-        val record = createAuditRecord(ts, ctx, response)
-        bufferedAuditWriteRecord(record)
-        response
+        val (attachRespCapture, respPromise) = shouldCaptureAndPromise(response.entity.contentLengthOption)
+
+        val respDataBytes = if (attachRespCapture) {
+          val sink = createCaptureSink(respPromise)
+          response.entity.dataBytes.alsoTo(sink)
+        } else {
+          response.entity.dataBytes
+        }
+
+        val modResponse = response.withEntity(
+          response.entity.contentLengthOption match {
+            case Some(0)    => response.entity
+            case Some(len)  => HttpEntity.Default(response.entity.contentType, len, respDataBytes)
+            case None       => HttpEntity        (response.entity.contentType, respDataBytes)
+          }
+        )
+
+        val auditF = for {
+          reqC <- reqPromise.future
+          respC <- respPromise.future
+        } yield {
+          val auditReq = ctx.req.withEntity(HttpEntity.Strict(ctx.req.entity.contentType, reqC))
+          val auditRes = response.withEntity(HttpEntity.Strict(response.entity.contentType, respC))
+          val auditCtx = ctx.copy(req = auditReq)
+          val record   = createAuditRecord(ts, auditCtx, auditRes)
+          bufferedAuditWriteRecord(record)
+        }
+
+        auditF.failed.foreach { ex =>
+          logger.error("Failed to audit", ex)
+        }
+
+        Success(modResponse)
       case Failure(ex) =>
         // Do nothing here. Use error handler to produce response and call audit from there.
-    }(ctx.as.dispatcher)
+        // TODO use captured request body somehow!
+        Failure(ex)
+    }
   }
 }
